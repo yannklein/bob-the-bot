ed68a0a8dfe5a1bcccb2cce2c86657b532d985d1
Yann Klein <yann.klein@me.com>
2019-06-27 08:57:50 +0900

    regex in jp real correction

--- app.rb
+++ app.rb
@@ -36,11 +36,11 @@ end
 def bot_jp_answer_to(a_question, user_name)
   if a_question.match?(/(おはよう|こんにちは|こんばんは|ヤッホー|ハロー).*/)
     "こんにちは#{user_name}さん！お元気ですか?"
-  elsif a_question.match?(/.*元気.*(？|?｜か)/)
+  elsif a_question.match?(/.*元気.*(？|\?｜か)/)
     "私は元気です、#{user_name}さん"
   elsif a_question.match?(/.*(le wagon|ワゴン|バゴン).*/i)
     "#{user_name}さん... もしかして京都のLE WAGONプログラミング学校の話ですかね？ 素敵な画っこと思います！"
-  elsif a_question.end_with?('(?|？|か)')
+  elsif a_question.end_with?('?','？','か')
     "いい質問ですね、#{user_nam}さん！"
   else
     ["そうですね！", "確かに！", "間違い無いですね！"].sample
