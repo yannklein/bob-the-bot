139d55aad4b2f836fd717446486996a7086fc04d
Yann Klein <yann.klein@me.com>
2020-07-17 16:24:24 +0900

    micro style change in the report

--- weather_api.rb
+++ weather_api.rb
@@ -10,9 +10,9 @@ def fetch_weather(message)
   location = message.match(/.+weather in (\w+).*/)[1]
 
   # Coordinates from keyword
-  # v
   coord = Geocoder.search(location).first.coordinates
   api_key = ENV["WEATHER_API"]
+  api_key = "f5191f73c320e67df9461049016befe3"
   url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{coord[0]}&lon=#{coord[1]}&exclude=current,minutely,hourly&appid=#{api_key}"
   begin
     data_serialized = open(url).read
@@ -21,24 +21,19 @@ def fetch_weather(message)
   end
   data = JSON.parse(data_serialized)['daily'][0..3]
 
-  weather_for = []
-  temp_for = []
   days = ["today", "tomorrow", (Date.today+ 2).strftime('%A'), (Date.today+ 3).strftime('%A')]
-  data.each_with_index do |day, index|
-    weather_for << [days[index], day['weather'][0]['main']]
-    temp_for << [days[index], day['temp']['day'] - 272.15]
-  end
-  freq = weather_for.map {|day| day[1]}.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+  weather_forcast = data.map_with_index { |day, index| [days[index], day['weather'][0]['main'], day['temp']['day'] - 272.15] }
+  freq = weather_forcast.map {|day| day[1]}.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
   most_freq_weather = freq.max_by{|k,v| v}[0]
 
   # Report creation
   report = "The weather is mostly #{most_freq_weather.upcase} in #{location} for the next 4 days.\n"
   # If there are particular weather days
-  other_weathers = weather_for.reject { |day| day[1] == most_freq_weather}
+  other_weathers = weather_forcast.reject { |day| day[1] == most_freq_weather}
   report += "Except on #{other_weathers.map { |day| "#{day[0]}(#{day[1]})" }.join(", ")}.\n" if other_weathers.any?
   #tempreatures
-  report += "\nThe temperature will be:\n#{temp_for.map {|day| "- #{day[1].round}˚C for #{day[0]}"}.join("\n")}"
-  {mostly: most_freq_weather, temps: temp_for, report: report}
+  report += "\nThe temperature will be:\n#{weather_forcast.map {|day| " #{day[2].round}˚C for #{day[0]}"}.join("\n")}"
+  {mostly: most_freq_weather, weather_forcast: weather_forcast, report: report}
 end
 
 p fetch_weather("What is the weather in Tokyo?")[:report]
