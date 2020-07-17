9707bdc940400135778536693cabc125cebe4d1c
Yann Klein <yann.klein@me.com>
2020-07-17 10:04:58 +0900

    put ibm watson into a separate folder

--- app.rb
+++ app.rb
@@ -4,11 +4,9 @@ require "json"
 require "net/http"
 require "uri"
 require "tempfile"
-
 require "line/bot"
-require "ibm_watson/visual_recognition_v3"
 
-include IBMWatson
+require_relative "ibm_watson"
 
 def client
   @client ||= Line::Bot::Client.new { |config|
@@ -18,6 +16,7 @@ def client
 end
 
 def bot_answer_to(a_question, user_name)
+  # Only answer to messages with "bob"
   return "" unless a_question.downcase.include?("bob")
 
   if a_question.match?(/(Hi|Hey|Bonjour|Hi there|Hey there|Hello).*/i)
@@ -58,28 +57,6 @@ def send_bot_message(message, client, event)
   p event["replyToken"]
   p message
   p client
-end
-
-def fetch_ibm_watson(response_image)
-  tf = Tempfile.open
-  tf.write(response_image.body)
-  # Using IBM Watson visual recognition API
-  visual_recognition = VisualRecognitionV3.new(
-    version: "2018-03-19",
-    iam_apikey: ENV["IBM_IAM_API_KEY"]
-  )
-  image_results = ""
-  File.open(tf.path) do |images_file|
-    classes = visual_recognition.classify(
-      images_file: images_file,
-      threshold: "0.6"
-    )
-    image_results = classes.result["images"][0]["classifiers"][0]["classes"]
-    image_results = image_results.map {|result| result["class"].upcase}
-  end
-  # Do something with the image results
-  yield(image_results)
-  tf.unlink
 end
 
 post "/callback" do
