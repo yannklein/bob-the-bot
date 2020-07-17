e02753b207b0294dbe8f3e9399d397c260413724
Yann Klein <yann.klein@me.com>
2020-07-17 17:35:40 +0900

    re style tokyo event message

--- tokyo_events_api.rb
+++ tokyo_events_api.rb
@@ -12,6 +12,15 @@ def fetch_tokyo_events
   data = JSON.parse(data_serialized)
   # Only keep the events of the week and sort'em
   week_events = data.reject {|event| Date.parse(event['date']) - Date.today > 7 }.sort_by {|event| event['date']}
-  week_events_formatted = week_events.map {|event| "#{Date.parse(event['date']).strftime('%a, %b %e')} - #{event['name']}" }
-  "There are some cool events this week:\n#{week_events_formatted.join("\n")}"
+  week_events_hash = Hash.new("")
+  week_events.each do |event|
+    date = Date.parse(event['date']).strftime('%a, %b %e')
+    week_events_hash[date] += "#{event['name']}"
+  end
+
+  answer = "There are some cool events this week:"
+  week_events_hash.each do |key, value|
+    answer += "\n\n- #{key}\n#{value}"
+  end
+  answer
 end
