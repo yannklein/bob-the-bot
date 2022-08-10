7049c6d978f6d221b700df6b028b924938a9d5ca
Yann Klein <kleinyann@MacBook-Air.local>
2022-08-10 16:00:11 +0900

    add some debugging logs for imagga

--- imagga.rb
+++ imagga.rb
@@ -20,12 +20,12 @@ def get_classes(images_file)
 
   auth = 'Basic ' + Base64.strict_encode64( "#{api_key}:#{api_secret}" ).chomp
   response = RestClient.post "https://api.imagga.com/v2/uploads", { :image => images_file }, { :Authorization => auth }
-  p response["result"]["upload_id"]
+  p "Image uploda ID:", response["result"]["upload_id"]
   image_upload_id = response["result"]["upload_id"]
 
   auth = 'Basic ' + Base64.strict_encode64( "#{api_key}:#{api_secret}" ).chomp
   response = RestClient.get "https://api.imagga.com/v2/tags?image_upload_id=#{image_upload_id}", { :Authorization => auth }
   classes = response["result"]["tags"].map { |tag| tag["tag"]["en"]}
-  p classes
+  p "Recognition result:", classes
   classes
 end
